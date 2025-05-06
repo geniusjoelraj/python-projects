5fd58d544b4543bd8c3dfe7e8287526303f66d5a
geniusjoelraj <geniusjoel2005@gmail.com>
2025-05-06 11:02:00 +0530

    Improved email-sender

--- email-sender/main.py
+++ email-sender/main.py
@@ -4,6 +4,7 @@ from dotenv import find_dotenv, load_dotenv
 from email.message import EmailMessage
 import ssl
 import smtplib
+from yaspin import yaspin
 
 # loading the password
 load_dotenv(find_dotenv())
@@ -11,12 +12,18 @@ PASS = os.getenv("EMAIL_PASS")
 
 # emial contents
 email_sender = "tadanotensai05@gmail.com"
-email_reciever ="tadanotensai05@gmail.com" 
+email_reciever ="geniusjoel2005@gmail.com" 
 email_password = PASS or ""
-subject = "testing email automation"
-body="""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque lectus eget elit ultricies gravida.
-"""
+subject = input("Subject: ")
+print("Body: (ctrl-d)")
+content=[]
+while True:
+    try:
+        line=input()
+    except EOFError:
+        break
+    content.append(line)
+body="\n".join(content)
 
 email = EmailMessage()
 email['From'] = email_sender
@@ -27,6 +34,12 @@ email.set_content(body)
 # making the connection secure
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
-    smtp.login(email_sender, email_password)
-    smtp.sendmail(email_sender, [email_reciever], email.as_string())
+with yaspin(text="Sending...") as spinner:
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, [email_reciever], email.as_string())
+        spinner.ok("󰩐")
+        print("Mail sent successfully")
+    except Exception:
+        spinner.fail("⤬")
